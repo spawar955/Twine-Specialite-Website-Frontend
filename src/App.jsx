b33ff18fb7691817784e9f8110ca2520c4941244
Shankar Pawar <spawar869089@gmail.com>
2024-03-12 01:17:44 +0530

    12-03-2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+// COMPONENTS 
 import Layout from "./components/Layout"
 import BreadCrum from "./components/BreadCrum"
 
+
+// PAGES 
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Contact from "./pages/Contact"
 import ShopNow from "./pages/ShopNow"
 import BuyNow from "./pages/BuyNow"
 import PaymentMethod from "./pages/PaymentMethod"
+
+
+// STYLES 
+import "./Styles/Header.css"
+import "./Styles/Footer.css"
+import "./Styles/Home.css"
+
 
 function App() {
   return (
